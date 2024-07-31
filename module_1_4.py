3101ddcfef581d466449de6c27a4111973af90a0
Aleksanochka <pearloffka@gmail.com>
2024-07-31 18:40:01 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+my_string = input('print something: ')
+print(len(my_string),
+      my_string.upper(),
+      my_string.lower(),
+      my_string.replace(' ', ''),
+      my_string[0], my_string[-1], sep='\n')
