2df7941f900d3a9ce0f418905d523d570007a7d6
Aleksanochka <pearloffka@gmail.com>
2024-07-31 17:09:26 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,9 @@
+  # Varialbles
+
+tasks_count = 12
+hours_spent = 1.5
+course_name = 'Python'
+average_task_time = hours_spent / tasks_count
+print('Курс: ', course_name, 'всего задач: ',
+      tasks_count, 'затрачено часов: ', hours_spent,
+      'среднее время выполнения ', average_task_time)
