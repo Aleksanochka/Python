01b0c4523684d448c96084933ce4653fd83100e0
Aleksanochka <pearloffka@gmail.com>
2024-07-31 17:48:44 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Aleksandra'
+age = 18
+is_student = True
+print('Name: ' + name,
+      'Age: ' + str(age), sep='\n')
+age = age + 19
+print('New age: ' + str(age),
+      'Is student: ' + str(is_student), sep='\n')
