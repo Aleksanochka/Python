87cbb6bc8d894e123cff9c481368b4cf5ef4609b
Aleksanochka <pearloffka@gmail.com>
2024-08-03 01:21:39 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -2,8 +2,8 @@ Twice_members = {'Jihyo': 1997, 'Nayeon ': 1995, 'Jeongyeon': 1996,
            'Momo': 1996, 'Sana': 1996, 'Mina': 1997,
            'Dahyun': 1998, 'Rose': 'Rose is not a Twice member'}
 print(Twice_members,
-      Twice_members['Jihyo'],
-      Twice_members.get('Chaeyoung', 'where are the others?'), sep='\n')
+      f"Jihyo's year of birth: {Twice_members['Jihyo']}",
+      f"Only 7 girls? {Twice_members.get('Chaeyoung', ' And where are the others?')}", sep='\n')
 
 Twice_members.update({'Chaeyoung': 1999, 'Tzuyu ': 1999})
 print(Twice_members.pop('Rose'))
